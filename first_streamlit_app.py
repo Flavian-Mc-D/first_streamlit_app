0bda9a6f7a4ca253e4b8f026dfeb898e2165ff07
Flavian Mc Donald <47428348+Flavian-Mc-D@users.noreply.github.com>
2022-04-26 18:07:40 -0300

    Update first_streamlit_app.py
    Fixed: streamlit.text((fruityvice_response).json())
    
    Added :
    #normalized answer
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    #output a table on the screen
    streamlit.dataframe(fruityvice_normalized)

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -26,4 +26,11 @@ streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text((fruityvice_response).json())
+streamlit.text((fruityvice_response).json()) # Writes de answer 
+
+#normalized answer
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output a table on the screen
+streamlit.dataframe(fruityvice_normalized)
+
+
