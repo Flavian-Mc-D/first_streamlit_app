5d24019a76f1a35a67b54125db3b1a377eee4181
Flavian Mc Donald <47428348+Flavian-Mc-D@users.noreply.github.com>
2022-05-10 11:40:00 -0300

    Update first_streamlit_app.py

--- first_streamlit_app.py
+++ first_streamlit_app.py
@@ -42,6 +42,12 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output a table on the screen
 streamlit.dataframe(fruityvice_normalized)
 
+
+streamlit.header('Would you like to pick another Fruit?')
+#Adding a second pick list:
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -50,8 +56,3 @@ my_cur.execute("SELECT * FROM fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
-
-streamlit.header('Would you like to pick another Fruit?')
-#Adding a second pick list:
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
